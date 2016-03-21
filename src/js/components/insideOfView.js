134346bdca1456775cbd517e1f4d5575608b25b7
Fabian <murillo.a.fabian@gmail.com>
2016-03-21 14:14:03 -0400

    newest scaffold

--- src/js/components/insideOfView.js
+++
@@ -1,9 +0,0 @@
-var InsideOfView = Backbone.View.extend({
-
-    template: _.template('<h1>I am inside a View!!!</h1>'),
-
-    render: function () {
-        this.$el.html(this.template());
-        return this;
-    }
-});
--- src/js/components/newStuffView.js
+++
@@ -1,8 +0,0 @@
-var NewStuffView = Backbone.View.extend({
-
-    template: _.template('<h1>New Stuff!</h1>'),
-
-    render: function () {
-        this.$el.html(this.template());
-    }
-});
